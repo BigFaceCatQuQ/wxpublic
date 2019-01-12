28d8316107b70384f8ff13857a6dbe5d6a4e651d
mayer <1224476825@qq.com>
2019-01-12 15:59:59 +0800

    wangzheng NMSL

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,44 +1,54 @@
 <template>
   <div class="home">
-    <div class="top">
-
-    </div>
-    <div class="centre">
-
-    </div>
-    <div class="bottom">
-
-    </div>
+    <el-header>
+      <div class="top">
+        <el-row>
+          <el-col :span="12">
+            <div style="blackground-color:red" class="grid-content bg-purple"> <img src="../assets/LOGO.png" /> </div>
+          </el-col>
+          <el-col :span="12">
+            <div style="blackground-color:black" class="grid-content bg-purple-light"></div>
+          </el-col>
+        </el-row>
+      </div>
+    </el-header>
+    <el-main>
+      <div class="centre"></div>
+    </el-main>
+    <el-footer>
+      <div class="bottom"></div>
+    </el-footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-}
+  name: "Home"
+};
 </script>
 
 <style scoped>
-.home{
-  width:100%;
+.home {
+  width: 100%;
   height: 100%;
 }
 
-.top{
+.top {
   width: 100%;
   height: 120px;
-  background-color: red;
+  background-color: #ffffff;
 }
 
-.centre{
-  width:100%;
+.centre {
+  width: 100%;
   height: 550px;
-  background-color: green;
+  background-color: #e8e8e8;
 }
 
-.bottom{
-    width:100%;
+.bottom {
+  width: 100%;
   height: 550px;
-  background-color: blue;
+  background-color: #1f2025;
 }
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home'
+import ElementUI from 'element-ui';
 
-Vue.use(Router)
+Vue.use(Router);
+Vue.use(ElementUI);
 
 export default new Router({
     routes:[
