7a788bbb1a1112e4a51725e966779973888df432
shikongfanjin <1902252314@qq.com>
2019-01-12 14:29:20 +0800

    ..

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div class="top">
+
+    </div>
+    <div class="centre">
+
+    </div>
+    <div class="bottom">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style scoped>
+.home{
+  width:100%;
+  height: 100%;
+}
+
+.top{
+  width: 100%;
+  height: 120px;
+  background-color: red;
+}
+
+.centre{
+  width:100%;
+  height: 550px;
+  background-color: green;
+}
+
+.bottom{
+    width:100%;
+  height: 550px;
+  background-color: blue;
+}
+</style>
