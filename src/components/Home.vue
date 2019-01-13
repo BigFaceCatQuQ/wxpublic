29cdd31b0f3ef158662831c10e696cf534abfa6d
shikongfanjin <1902252314@qq.com>
2019-01-13 10:47:02 +0800

    顶部及残缺的轮播

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,55 @@
 <template>
   <div class="home">
-    <el-header>
+    <el-header height=120px>
       <div class="top">
-          <div class="head-left">
-            <div style="blackground-color:red" class="grid-content bg-purple"> <img src="../assets/LOGO.png"/> </div>
+        <div class="head-left">
+          <div class="logo-img">
+            <img
+              src="../assets/logo1.png"
+              alt=""
+              height="120px"
+            >
           </div>
-          <div class="head-right">
-            <div style="blackground-color:black" class="grid-content bg-purple-light"></div>
+          <div class="word">
+            <div style="padding:30px 0 0 0;text-align:left">
+              遥可及 远可信
+            </div>
+            <div style="padding:10px 0 0 0;text-align:left">
+              远信——实现作业管理的远程化、透明化
+            </div>
           </div>
+        </div>
+        <div class="head-right">
+          <div class="tag">
+            首页
+          </div>
+          <div class="tag">
+            代理招商说明
+          </div>
+          <div class="tag">
+            代理合同文本
+          </div>
+        </div>
       </div>
     </el-header>
     <el-main>
-      <div class="centre"></div>
+      <div class="centre">
+        <el-carousel
+          arrow="always"
+          trigger="click"
+          height="550px"
+        >
+          <el-carousel-item
+            v-for="item in imgList"
+            :key="item.id"
+          >
+            <el-col :span="24"><img
+                :src="item.idView"
+                width="100%"
+              /></el-col>
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </el-main>
     <el-footer>
       <div class="bottom"></div>
@@ -21,7 +59,15 @@
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      imgList: [
+        { id: 0, idView: require("../assets/lunbo1.png") },
+        { id: 1, idView: require("../assets/lunbo2.png") }
+      ]
+    };
+  }
 };
 </script>
 
@@ -50,10 +96,52 @@ export default {
 }
 
 .head-left {
-    float: left;
+  float: left;
+  padding-left: 80px;
+  width: 50%;
+  height: 120px;
 }
 
 .head-right {
-    float: right;
+  float: right;
+  width: 50%;
+  height: 120px;
+}
+
+.logo-img {
+  float: left;
+  width: 170px;
+}
+
+.word {
+  float: left;
+  height: 120px;
+  font-size: 20px;
+}
+
+.tag {
+  width: 24%;
+  float: left;
+  padding-top: 50px;
+  font-size: 20px;
 }
 </style>
+
+<style>
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
+
