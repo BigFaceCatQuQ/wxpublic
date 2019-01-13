cf98d9ed6b75371317ee6cf72e2b814269866b8f
shikongfanjin <1902252314@qq.com>
2019-01-13 16:41:27 +0800

    招商说明

--- src/components/Contract.vue
+++ src/components/Contract.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="contract">
-3333333333333
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>远信市场开发代理合同</span>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+      </div>
+    </el-card>
+
   </div>
 </template>
 
@@ -8,19 +16,38 @@
 export default {
   name: "Contract",
   data() {
-    return {
-    };
+    return {};
   },
   methods: {},
-  mounted() {
-
-  }
+  mounted() {}
 };
 </script>
 
 <style scoped>
-.contract{
-    width: 100%;
-    height: 100%;
+.contract {
+  width: 100%;
+  height: 100%;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both;
+}
+
+.box-card {
+  width: 80%;
+  margin: 0 10%;
 }
 </style>
